fb65b253dee5216af6c725ab276c08df3f7db6b7
bobur <bobur@gmail.com>
2024-05-11 06:44:55 +0500

    inputs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,6 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="footer footer-center p-5 bg-base-200 text-base-content rounded">
-      <nav className="grid grid-flow-col gap-4">
-        <Link to="/about" className="link link-hover">
-          About us
-        </Link>
-        <Link to="/contact" className="link link-hover">
-          Contact
-        </Link>
-        <Link to="/create" className="link link-hover">
-          Create
-        </Link>
-      </nav>
-
       <aside>
         <p>
           Copyright © 2024 - All right reserved by ACME Industries Ltd{" "}
